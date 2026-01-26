48dfc3625a499e98a5ca7b93c6bec0ad89ce2fc8
Alexander Castronovo <alexandercastronovo@gmail.com>
2026-01-26 13:38:42 -0500

    skip any spines if there's an error

--- pipeline/headcenters/preprocessingworker.py
+++ pipeline/headcenters/preprocessingworker.py
@@ -53,32 +53,37 @@ class PreprocessingWorker(QThread):
 
             log.info("Iterating over spines to compute radii as a function of path length")
             for idx, (spine_skeleton, spine_radii) in enumerate(zip(spine_skeletons, radii)):
-                self.update_label.emit(f"Computing head radius for {idx + 1}/{total_length} spines")
-                spacing = 5  # nm
+                try:
+                    self.update_label.emit(f"Computing head radius for {idx + 1}/{total_length} spines")
+                    spacing = 5  # nm
 
-                log.info(f"Spine {idx}: computing radii")
-                points_tangents, radii_tangents = skeleton_helper.get_radius_polyline(
-                    spine_skeleton[::-1], tm_mesh, n_rays=140,
-                    aggregate='mean', projection='tangents', path_interpolation_spacing=spacing,
-                    fallback=None
-                )
+                    log.info(f"Spine {idx}: computing radii")
+                    points_tangents, radii_tangents = skeleton_helper.get_radius_polyline(
+                        spine_skeleton[::-1], tm_mesh, n_rays=140,
+                        aggregate='mean', projection='tangents', path_interpolation_spacing=spacing,
+                        fallback=None
+                    )
 
-                log.info(f"Spine {idx}: {len(points_tangents)} points for head radius computation")
+                    log.info(f"Spine {idx}: {len(points_tangents)} points for head radius computation")
 
-                cumulative_points = geom.accumulate(points_tangents)
+                    cumulative_points = geom.accumulate(points_tangents)
+                    log.info(f"Spine {idx}: Length is {cumulative_points[-1]} nm")
 
-                log.info(f"Spine {idx}: Computing head point")
-                head_point_3d = sa.find_head_point(
-                    spine_skeleton[::-1],
-                    cumulative_points,
-                    radii_tangents,
-                    0.004,
-                    # f"out/headradius/spine_graph/spine_head_{idx}.npz"
-                    None
-                )
-                head_center_points.append(head_point_3d)
+                    log.info(f"Spine {idx}: Computing head point")
+                    head_point_3d = sa.find_head_point(
+                        spine_skeleton[::-1],
+                        cumulative_points,
+                        radii_tangents,
+                        0.004,
+                        # f"out/headradius/spine_graph/spine_head_{idx}.npz"
+                        None
+                    )
+                    head_center_points.append(head_point_3d)
 
-                log.info(f"Spine {idx}: Head point at {head_point_3d}")
+                    log.info(f"Spine {idx}: Head point at {head_point_3d}")
+                except Exception:
+                    log.error(f"An error occurred while processing spine {idx}. Skipping to next spine.")
+                    log.error(traceback.format_exc())
 
             self.update_label.emit("Creating head centers annotation")
             log.info("Creating head centers annotation")
